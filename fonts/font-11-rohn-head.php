37c03367ed2c922b187c1e5002103a5f809d93bc
Frank Bültge <frank@bueltge.de>
2013-03-04 21:22:25 +0100

    fixed #1, ad this new fonsts with new section

---
+++ fonts/font-11-rohn-head.php
@@ -0,0 +1,3 @@
+<link href='http://fonts.googleapis.com/css?family=Nixie+One' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Ledger' rel='stylesheet' type='text/css'>
+<link href="fonts/font-11-rohn-style.css" rel="stylesheet" type="text/css" media="screen">
---
+++ fonts/font-11-rohn-section.php
@@ -0,0 +1,17 @@
+
+<section class="sample" id="rohn">
+	<h1 class="hidden">Fonts: Nixie One + Ledger</h1>
+	
+	<section>
+		<a href="http://www.google.com/webfonts/specimen/Nixie+One" title="Nixie One">
+			<h1>Jim Rohn</h1>
+			<h2>American entrepreneur, author</h2>
+		</a>
+		<a href="http://www.google.com/webfonts/specimen/Ledger" title="Ledger">
+			<blockquote cite="Richard Branson">
+				<p>Poor people have big TV. Rich people have big library.</p>
+			</blockquote>
+		</a>
+	</section>
+	
+</section>
