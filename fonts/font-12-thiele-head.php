4c4b3adf570c81a7f3f03164b6f2639b4d5cca2e
Frank Bültge <frank@bueltge.de>
2016-01-05 14:37:55 +0100

    Add new section, fixed #5

---
+++ fonts/font-12-thiele-head.php
@@ -0,0 +1,2 @@
+<link href='https://fonts.googleapis.com/css?family=Fira+Sans:400,700,700italic,400italic,300,300italic|Bree+Serif' rel='stylesheet' type='text/css'>
+<link href="fonts/font-10-thiele-style.css" rel="stylesheet" type="text/css" media="screen">
---
+++ fonts/font-12-thiele-section.php
@@ -0,0 +1,17 @@
+
+<section class="sample" id="thiele">
+	<h1 class="hidden">Fonts: Bree and Fira</h1>
+	
+	<section>
+		<a href="https://www.google.com/fonts/specimen/Bree+Serif" title="Bree Sans">
+			<h1>Marc Thiele</h1>
+		</a>
+		<a href="https://www.google.com/fonts/specimen/Fira+Sans" title="Fira Sans">
+			<blockquote cite="Marc Thiele">
+				<p>When I was younger, I never really was interested in Computers. OK, games like Sid Meier's Pirates!, Defender of the Crown, or To be on Top were games I loved to play on the C64.</p>
+				<p>Today I run beyond tellerrand, an event for the (web) community. I do this full-time and as a one-man-band ever since it started and I love it.</p>
+			</blockquote>
+		</a>
+	</section>
+	
+</section>
